324d9e81d2aca0f645e5a36e499f508a9fea4123
Ruslan Khanov <hanov@technokratos.com>
2021-11-14 13:52:28 +0300

    init project

---
+++ CombineHomework/CombineHomework/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CombineHomework
+//
+//  Created by Ruslan Khanov on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Instance methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
