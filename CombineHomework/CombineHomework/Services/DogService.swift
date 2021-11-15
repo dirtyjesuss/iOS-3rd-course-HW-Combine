3457b1a8dd78faf3ff29ab29d3b8cf3bbb4b1c41
Ruslan Khanov <hanov@technokratos.com>
2021-11-15 22:57:08 +0300

    fetch dogs, counter

---
+++ CombineHomework/CombineHomework/Services/DogService.swift
@@ -0,0 +1,66 @@
+//
+//  DogService.swift
+//  CombineHomework
+//
+//  Created by Ruslan Khanov on 15.11.2021.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+final class DogService {
+
+    // MARK: - Instance properties
+
+    @Published var counter: Int = 0
+
+    var publisher: AnyPublisher<Data, Never> {
+        subject.eraseToAnyPublisher()
+    }
+
+    private let subject = PassthroughSubject<Data, Never>()
+
+    private let decoder = JSONDecoder()
+    private let session = URLSession.shared
+    private var loadDogImageDataCancellable: AnyCancellable?
+    private var fetchDogResponseCancellable: AnyCancellable?
+
+    private var url: URL {
+        guard let url = URL(string: "https://dog.ceo/api/breeds/image/random") else {
+            fatalError("Base url is not configured")
+        }
+
+        return url
+    }
+
+    // MARK: - Instance methods
+
+    func loadDogImageData() {
+        fetchDogResponseCancellable = fetchDogResponse()
+            .sink(
+                receiveCompletion: { _ in},
+                receiveValue: { [weak self] url in
+                    guard let self = self else { return }
+
+                    self.loadDogImageDataCancellable = self.session.dataTaskPublisher(for: url)
+                        .map(\.data)
+                        .replaceError(with: Data())
+                        .sink { data in
+                            self.counter += 1
+                            self.subject.send(data)
+                        }
+                }
+            )
+    }
+
+    private func fetchDogResponse() -> AnyPublisher<URL, Error> {
+        return session.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: DogResponse.self, decoder: decoder)
+            .compactMap {
+                URL(string: $0.message)
+            }
+            .eraseToAnyPublisher()
+    }
+}
